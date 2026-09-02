8c0cb4d1d84661ae82474cc77d78a7d3566fe0a8
During15 <faithduring16@gmail.com>
2026-09-02 23:54:08 +0200

    feat: add role entity

---
+++ backend/src/main/java/com/during15/school/model/Role.java
@@ -0,0 +1,45 @@
+package com.during15.school.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Column(name = "role_name", nullable = false, unique = true)
+    private String roleName;
+
+    private String description;
+
+    public Role() {
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
