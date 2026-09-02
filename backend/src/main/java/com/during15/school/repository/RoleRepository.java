3837982c4de4553e7088d9474df9149a5b76a629
During15 <faithduring16@gmail.com>
2026-09-02 23:59:53 +0200

    feat: add role repository

---
+++ backend/src/main/java/com/during15/school/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.during15.school.repository;
+
+import com.during15.school.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
