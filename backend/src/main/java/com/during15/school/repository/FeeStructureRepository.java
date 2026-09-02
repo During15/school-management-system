3cc7f0e52ae35612cb93d6e65256bf83f5124493
During15 <faithduring16@gmail.com>
2026-09-03 00:57:32 +0200

    feat: add fee structure repository

---
+++ backend/src/main/java/com/during15/school/repository/FeeStructureRepository.java
@@ -0,0 +1,7 @@
+package com.during15.school.repository;
+
+import com.during15.school.model.FeeStructure;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeeStructureRepository extends JpaRepository<FeeStructure, Long> {
+}
